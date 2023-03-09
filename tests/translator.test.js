492873677306077ca658cc44cb090ebea7f80e76
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-03-09 09:36:54 +0100

    Added example test for CI/CD

--- tests/translator.test.js
+++ tests/translator.test.js
@@ -4,3 +4,15 @@
  * @author    Created by Vivien.Piccin
  * @version   08-03-2023 - original
  */
+"use strict";
+
+test('example_test', () => {
+    //given
+    let number = 10;
+
+    //when
+    //Assertion will trigger the events
+
+    //then
+    expect(number).toEqual(10);
+})
