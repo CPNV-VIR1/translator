d17f34316f31794e18f2ba345c614b0d27922007
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-03-29 09:31:37 +0200

    Added backend translation logic

--- index.js
+++ index.js
@@ -8,19 +8,27 @@
 
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const config = require('./config/' + (process.env.NODE_ENV || 'development') + '.js');
+const translator = require('./app/translator/translator');
+const {response} = require("express");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public'), {etag: false}));
+app.use(bodyParser.json());
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (request, response) => {
-    response.render('index.html');
+    response.render('index');
 });
+
+app.post('/translate', (request, response) => {
+    response.send(translator.translate(request.body.text, request.body.language));
+})
 
 app.listen(config.Host.port || 8080, () => {
     console.log(`Translatator started on port: ${config.Host.port || 8080}`);
