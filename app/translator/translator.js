7668d5057d3ebb4a0ec45b296060f4e9cf9b233a
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-03-30 10:50:21 +0200

    Fixed seedrandom always using the same seed

--- app/translator/translator.js
+++ app/translator/translator.js
@@ -12,17 +12,18 @@ module.exports = class Translator {
     static translate(text, language = 'ip') {
         let textHash = crypto.createHash('sha256');
         textHash.update(text);
+        seedrandom(textHash.digest('base64'), { global: true});
         return text.split(' ').map(element => {
             try {
-                return dictionary[language][this.generateRandom(textHash.digest('base64'),0, dictionary[language].length)];
+                return dictionary[language][this.generateRandom(0, dictionary[language].length)];
             } catch (ex) {
                 //Need to raise some sort of error
+                console.log('Error raised !', ex.toString());
                 return element;
             }
         }).toString().replace(/,/g, ' ');
     }
-    static generateRandom(seed = 'seed', min = 0, max = 100) {
-        let random = seedrandom(seed);
-        return Math.floor(random() * (max - min)) + min;
+    static generateRandom(min = 0, max = 100) {
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 }
