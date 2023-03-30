892c37b23b828ab699c74dc558eac4d6877ff87c
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-03-30 08:18:24 +0200

    Fixed faulty translation
    The error was due to not updating the hash on crypto, causing the same
    hash to always be generated. Thus outputing the same translation every
    time

--- app/translator/translator.js
+++ app/translator/translator.js
@@ -10,18 +10,19 @@ const seedrandom = require("seedrandom");
 const dictionary = require("./dictionary.json");
 module.exports = class Translator {
     static translate(text, language = 'ip') {
-        let textHash = crypto.createHash('sha256', text);
-        seedrandom(textHash.digest('base64'), {global: true});
+        let textHash = crypto.createHash('sha256');
+        textHash.update(text);
         return text.split(' ').map(element => {
             try {
-                return dictionary[language][this.generateRandom(0, dictionary[language].length)];
+                return dictionary[language][this.generateRandom(textHash.digest('base64'),0, dictionary[language].length)];
             } catch (ex) {
                 //Need to raise some sort of error
                 return element;
             }
         }).toString().replace(/,/g, ' ');
     }
-    static generateRandom(min = 0, max = 100) {
-        return Math.floor(Math.random() * (max - min)) + min;
+    static generateRandom(seed = 'seed', min = 0, max = 100) {
+        let random = seedrandom(seed);
+        return Math.floor(random() * (max - min)) + min;
     }
 }
