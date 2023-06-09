5d57cd9afb12ce91c7e75be767f4901a8beb834d
Vivien Piccin <Vivien.Piccin@cpnv.ch>
2023-06-09 11:15:07 +0200

    Removed frontend routes

--- index.js
+++ index.js
@@ -6,20 +6,12 @@
  */
 
 
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mustacheExpress = require('mustache-express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public'), {etag: false}));
 app.use(bodyParser.json());
-app.use('/', require('./routes/index'));
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'resources/views'));
-app.use('/', require('./routes/index'));
 app.use('/translation', require('./routes/translation'));
 
 if (process.env.NODE_ENV != 'test') {
