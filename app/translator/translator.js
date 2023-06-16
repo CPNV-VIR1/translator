52afcc612d23407270e1be023528232f05575351
Vivien Piccin <Vivien.Piccin@cpnv.ch>
2023-06-16 11:18:03 +0200

    Added database integration in project

--- app/translator/translator.js
+++ app/translator/translator.js
@@ -8,12 +8,14 @@
 const crypto = require("crypto");
 const seedrandom = require("seedrandom");
 const dictionary = require("./dictionary.json");
+const historyRecord = require("../../models/history.model");
+
 module.exports = class Translator {
     static translate(text, language = 'ip') {
         let textHash = crypto.createHash('sha256');
         textHash.update(text);
         seedrandom(textHash.digest('base64'), { global: true});
-        return text.split(' ').map(element => {
+        let translation = text.split(' ').map(element => {
             try {
                 return dictionary[language][this.generateRandom(0, dictionary[language].length)];
             } catch (ex) {
@@ -22,6 +24,11 @@ module.exports = class Translator {
                 return element;
             }
         }).toString().replace(/,/g, ' ');
+        historyRecord.create({
+            input: text,
+            output: translation
+        })
+        return translation;
     }
     static generateRandom(min = 0, max = 100) {
         return Math.floor(Math.random() * (max - min)) + min;
--- models/history.model.js
+++ models/history.model.js
@@ -1,5 +1,25 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('../utils/database');
+// Connection to DB
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+   {
+     host: process.env.DATABASE_HOST,
+     dialect: process.env.DATABASE_TYPE,
+     define:{
+       timestamps: true,
+     },
+   }
+ );
+ 
+// For testing the connection to DB
+
+sequelize.authenticate().then(()=>{
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database:', error);
+})
 
 const history = sequelize.define('History', {
   // Model attributes are defined here
@@ -23,3 +43,11 @@ const history = sequelize.define('History', {
     defaultValue: Sequelize.NOW,
   },
 });
+
+sequelize.sync().then(() => {
+  console.log('history table created successfully');
+}).catch((error) => {
+  console.error('Unable to create table: ', error);
+});
+
+module.exports = history
